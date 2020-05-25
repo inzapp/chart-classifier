6d9890afd4c681f786651f06437d92c2a37a6595
inzapp <inzapp@naver.com>
2020-05-25 16:38:58 +0900

    i: relatively x, y position

---
+++ test.py
@@ -0,0 +1,46 @@
+import cv2
+
+w = 41
+h = 20
+dose_h_offset = 23
+
+pos = {}
+pos['ref'] = 150
+pos['pre'] = 221
+pos['lv1'] = 287
+pos['lv2'] = 354
+pos['lv3'] = 421
+pos['lv4'] = 488
+pos['lv5'] = 554
+pos['lv6'] = 620
+
+pos['fvc_dose'] = 288
+pos['fvc_liters'] = 308
+pos['fvc_pref'] = 331
+pos['fvc_pchg'] = 351
+
+pos['fev1_dose'] = 382
+pos['fev1_liters'] = 403
+pos['fev1_pref'] = 422
+pos['fev1_pchg'] = 444
+
+pos['fef_25_75_dose'] = 473
+pos['fef_25_75_per'] = 494
+pos['fef_25_75_pref'] = 519
+pos['fef_25_75_pchg'] = 539
+
+pos['pef_dose'] = 564
+pos['pef_l_sec'] = 585
+pos['pef_pref'] = 605
+pos['pef_pchg'] = 626
+
+img = cv2.imread('1.jpg', cv2.IMREAD_COLOR)
+for i in range(1, 6 + 1):
+    cv2.imshow('img', img[pos['fvc_liters']:pos['fvc_liters'] + h, pos['lv' + str(i)]:pos['lv' + str(i)] + w])
+    cv2.waitKey(0)
+
+for i in range(1, 6 + 1):
+    cv2.imshow('img', img[pos['fvc_dose']:pos['fvc_dose'] + h, pos['lv' + str(i)] - dose_h_offset :pos['lv' + str(i)] + w])
+    cv2.waitKey(0)
+    
+cv2.destroyAllWindows()
