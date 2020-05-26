828a230e725aa8f4d4b81fddddaf2a5f142f519e
inzapp <inzapp@naver.com>
2020-05-26 20:42:20 +0900

    c: table image to string all

--- test.py
+++ test.py
@@ -6,27 +6,25 @@ pytesseract.pytesseract.tesseract_cmd = 'E:/Tesseract-OCR/tesseract.exe'
 template = cv2.imread('methacholine_template.jpg', cv2.IMREAD_COLOR)
 tpe = ThreadPoolExecutor(8)
 
-def detect(chart, template, ratio):
-    proc = cv2.resize(chart, dsize=(0, 0), fx=ratio, fy=ratio, interpolation=cv2.INTER_CUBIC)
-    res = cv2.matchTemplate(proc, template, cv2.TM_CCOEFF)
+def detect(image, template, ratio):
+    resized = cv2.resize(image, dsize=(0, 0), fx=ratio, fy=ratio, interpolation=cv2.INTER_CUBIC)
+    res = cv2.matchTemplate(resized, template, cv2.TM_CCOEFF)
     (_, max_val, _, max_loc) = cv2.minMaxLoc(res)
-    print(max_val)
-    print(ratio)
-    return [max_val, max_loc, proc]
+    # print(max_val)
+    # print(ratio)
+    return [max_val, max_loc, resized]
 
 
-def get_methacholine_aridol_table(img_file_name):
-    chart = cv2.imread(img_file_name, cv2.IMREAD_COLOR)
+def get_max_matched_res(image, template):
     ratio = 0.980
-    h, w, ch = template.shape
-    max_val = -99999999
-    max_loc = 0
-    max_chart = 0
+    max_val = -1
+    max_loc = -1
+    max_chart = -1
 
     fs = []
     ratio = 0.980
-    for i in range(1, 8):
-        fs.append(tpe.submit(detect, chart, template, ratio))
+    for i in range(1, 8 + 1):
+        fs.append(tpe.submit(detect, image, template, ratio))
         ratio += 0.005
 
     max_template_match_val  = -1
@@ -38,16 +36,28 @@ def get_methacholine_aridol_table(img_file_name):
             max_template_match_val = res[0]
             max_template_match_loc = res[1]
             max_template_match_img = res[2]
+    
+    return [max_template_match_img, max_template_match_loc]
 
-    table_x = max_template_match_loc[0]
-    table_y = max_template_match_loc[1] + h
+
+def get_methacholine_aridol_table(image, template):
+    res = get_max_matched_res(image, template)
+    img = res[0]
+    loc = res[1]
+    h, w, ch = template.shape
+    table_x = loc[0]
+    table_y = loc[1] + h
     table_w = w
     table_h = 360
-    table = chart[table_y:table_y+table_h, table_x:table_x+table_w]
-    cv2.imshow('table', table)
-    cv2.waitKey(0)
+    table = img[table_y:table_y+table_h, table_x:table_x+table_w]
+    return table
 
 
-get_methacholine_aridol_table('0.jpg')
-        
+img = cv2.imread('1.jpg', cv2.IMREAD_COLOR)
+table = get_methacholine_aridol_table(img, template)
+ocr_res = pytesseract.image_to_string(table, config='-psm 6 digits')
+print(ocr_res)
+cv2.imshow('table', table)
+cv2.waitKey(0)
+
 
