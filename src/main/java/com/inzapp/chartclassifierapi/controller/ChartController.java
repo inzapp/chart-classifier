1173a01bd81cfeec38d6c52e3c8ff5de3780c757
inzapp <inzapp@naver.com>
2020-06-10 16:04:52 +0900

    API POST REFACTOR

--- src/main/java/com/inzapp/chartclassifierapi/controller/ChartController.java
+++ src/main/java/com/inzapp/chartclassifierapi/controller/ChartController.java
@@ -10,11 +10,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,30 +20,39 @@ import org.springframework.web.bind.annotation.RestController;
 public class ChartController {
 	private final String[] AVAILABLE_OPTIONS = { "grayscale", "threshold", "blur", "invert" };
 
-	@GetMapping("ocr")
-	public Map<String, Object> ocr(@RequestParam("path") String path, HttpServletRequest request) throws Exception {
+	@SuppressWarnings("unchecked")
+	@PostMapping("ocr")
+	public Map<String, Object> ocr(@RequestBody Map<String, Object> params) throws Exception {
 		Map<String, Object> map = new HashMap<>();
+		List<String> paths = (List<String>) params.get("path");
+		if (paths == null || paths.size() == 0) {
+			map.put("result", "failure: need path parameter");
+			return map;
+		}
+		List<String> options = (List<String>) params.get("option");
 
 		// check path is valid
-		String pathValidRes = this.checkPathIsValid(path);
+		String pathValidRes = this.checkPathIsValid(paths);
 		if (!pathValidRes.equals("success")) {
 			map.put("result", pathValidRes);
 			return map;
 		}
 
 		// replace regex
-		path.replaceAll("/", "\\");
+		for (int i = 0; i < paths.size(); ++i) {
+			paths.set(i, paths.get(i).replaceAll("/", "\\"));
+		}
 
 		// check option is valid
-		String option = request.getParameter("option") != null ? request.getParameter("option") : "";
-		String optionValidRes = this.checkOptionIsValid(option);
+		String optionValidRes = this.checkOptionIsValid(options);
 		if (!optionValidRes.equals("success")) {
 			map.put("result", optionValidRes);
 			return map;
 		}
 
 		// run python process and wait until end
-		Process p = Runtime.getRuntime().exec(String.format("python ocr.py path=%s option=%s", path, option));
+		Process p = Runtime.getRuntime().exec(String.format("python ocr.py path=%s option=%s",
+				this.pathsToString(paths), this.optionsToString(options)));
 		p.waitFor();
 		p.destroy();
 
@@ -55,39 +62,36 @@ public class ChartController {
 			map.put("result", "failure");
 		} else {
 			map.put("result", "success");
-			String[] paths = path.split("*");
-			for (String curPath : paths) {
+			for (String path : paths) {
 				Map<String, Object> curResultMap = new HashMap<>();
-				curResultMap.put("progress", this.getProgressFilePathList(curPath, option));
-				curResultMap.put("ocrResult", this.getOcrResult(curPath));
-				map.put(curPath, curResultMap);
+				curResultMap.put("progress", this.getProgressFilePathList(path, options));
+				curResultMap.put("ocrResult", this.getOcrResult(path));
+				map.put(path, curResultMap);
 			}
 		}
 		return map;
 	}
 
-	private String checkPathIsValid(String path) {
-		String[] paths = path.split("*");
-		for (int i = 0; i < paths.length; ++i) {
-			File pathFile = new File(paths[i]);
+	private String checkPathIsValid(List<String> paths) {
+		for (String path : paths) {
+			File pathFile = new File(path);
 			if (!pathFile.exists()) {
-				return "file not found : " + paths[i];
+				return "file not found : " + path;
 			} else if (!pathFile.isFile()) {
-				return "is not file : " + paths[i];
+				return "is not file : " + path;
 			}
 		}
 		return "success";
 	}
 
-	private String checkOptionIsValid(String option) {
-		if (option.equals("")) {
+	private String checkOptionIsValid(List<String> options) {
+		if (options == null) {
 			return "success";
 		}
-		String[] options = option.split("*");
-		for (int i = 0; i < options.length; ++i) {
+		for (String option : options) {
 			boolean isAvailable = false;
-			for (int j = 0; j < AVAILABLE_OPTIONS.length; ++j) {
-				if (options[i].equals(AVAILABLE_OPTIONS[j])) {
+			for (String availableOption : AVAILABLE_OPTIONS) {
+				if (option.equals(availableOption)) {
 					isAvailable = true;
 					break;
 				}
@@ -101,22 +105,50 @@ public class ChartController {
 					}
 					availableOptions.append(", ");
 				}
-				return String.format("invalid option : %s. you can only use %s", options[i],
-						availableOptions.toString());
+				return String.format("invalid option : %s. you can only use %s", option, availableOptions.toString());
 			}
 		}
 		return "success";
 	}
 
-	private List<String> getProgressFilePathList(String path, String option) throws Exception {
+	private String pathsToString(List<String> paths) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < paths.size(); ++i) {
+			sb.append(paths.get(i));
+			if (i == paths.size() - 1) {
+				break;
+			}
+			sb.append("*");
+		}
+		return sb.toString();
+	}
+
+	private String optionsToString(List<String> options) {
+		if (options == null) {
+			return "";
+		}
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < options.size(); ++i) {
+			sb.append(options.get(i));
+			if (i == options.size() - 1) {
+				break;
+			}
+			sb.append("*");
+		}
+		return sb.toString();
+	}
+
+	private List<String> getProgressFilePathList(String path, List<String> options) throws Exception {
+		if (options == null) {
+			return new ArrayList<>();
+		}
 		String[] sp = path.split("\\\\");
 		String fileName = sp[sp.length - 1];
 		String rawFileName = fileName.split("\\.")[0];
 		List<File> progressFiles = new LinkedList<>();
-		String[] options = option.split("*");
-		for (int i = 0; i < options.length; ++i) {
-			progressFiles
-					.add(new File(String.format("progress\\%s_%s_%s.jpg", rawFileName, String.valueOf(i), options[i])));
+		for (int i = 0; i < options.size(); ++i) {
+			progressFiles.add(
+					new File(String.format("progress\\%s_%s_%s.jpg", rawFileName, String.valueOf(i), options.get(i))));
 		}
 		List<String> progressFilePaths = new ArrayList<>();
 		for (File progressFile : progressFiles) {
